7d784222b3bcc343631db060a567730db7048f15
alhendrickson <159636032+alhendrickson@users.noreply.github.com.>
2025-06-02 16:57:40 +0000

    Setup readthedocs build

---
+++ observability/docs/conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Observability'
+copyright = '2025, CogStack Org'
+author = 'CogStack Org'
+release = 'latest'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx_rtd_theme',
+    'sphinx.ext.autodoc',
+    'myst_parser',
+    'sphinx.ext.inheritance_diagram',
+]
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
