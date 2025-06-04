5de738e95f2425c5b09b4b81a98843600b6d1308
alhendrickson <159636032+alhendrickson@users.noreply.github.com.>
2025-06-04 14:51:19 +0000

    Run Grafana alloy to replace exporters

---
+++ observability/build-docker.sh
@@ -0,0 +1,8 @@
+
+docker build -t cogstacksystems/cogstack-observability-prometheus:latest -f prometheus/Dockerfile.prometheus ./prometheus
+
+docker build -t cogstacksystems/cogstack-observability-blackbox-exporter:latest -f prometheus/Dockerfile.blackbox ./prometheus
+
+docker build -t cogstacksystems/cogstack-observability-grafana:latest -f grafana/Dockerfile ./grafana
+
+docker build -t cogstacksystems/cogstack-observability-traefik:latest -f traefik/Dockerfile ./traefik
