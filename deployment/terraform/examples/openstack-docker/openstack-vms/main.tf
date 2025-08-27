482fd5b840a8433c1c32036a01cb06914df4a5c0
alhendrickson <159636032+alhendrickson@users.noreply.github.com.>
2025-08-27 10:33:42 +0000

    bug(iac): Fix Openstack docker VMs always recreating due to bcrypt

--- deployment/terraform/examples/openstack-docker/openstack-vms/main.tf
+++ deployment/terraform/examples/openstack-docker/openstack-vms/main.tf
@@ -2,11 +2,8 @@ module "openstack_cogstack_infra" {
   source = "../../../modules/openstack-cogstack-infra"
   host_instances = [
     { name = "cogstack-devops-toolkit", is_controller = true },
-    #{ name = "medcat-nlp" }
+    { name = "medcat-nlp" }
   ]
   allowed_ingress_ips_cidr = var.allowed_ingress_ips_cidr
   ubuntu_immage_name       = var.ubuntu_immage_name
-  portainer_secrets = {
-    admin_password = "test"
-  }
 }
