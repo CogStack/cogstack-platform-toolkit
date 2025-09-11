b07cb0daf8e136821ed37b7ada9024bff9badc31
alhendrickson <159636032+alhendrickson@users.noreply.github.com.>
2025-09-11 11:05:08 +0000

    ops: Created Medcat trainer helm. Add github action. Cleanup

--- deployment/kubernetes/local_dev_startup.sh
+++ deployment/kubernetes/local_dev_startup.sh
@@ -23,3 +23,5 @@ helm test medcat-service --logs
 
 helm upgrade my-test ./medcat-trainer-helm --install --recreate-pods  --wait --timeout 5m0s # Install if it doesnt already exist, else upgrade
 # kubectl port-forward svc/medcat-trainer-solr 8983:8983
+
+## helm install trainer-registry oci://registry-1.docker.io/cogstacksystems/medcat-trainer-helm --wait --timeout 5m0s
