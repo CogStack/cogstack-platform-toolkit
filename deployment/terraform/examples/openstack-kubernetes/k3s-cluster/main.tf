349b57da99a764c69a3bf986c323a3a9b07905f9
alhendrickson <159636032+alhendrickson@users.noreply.github.com.>
2026-01-14 13:57:00 +0000

    feat(iac): format

--- deployment/terraform/examples/openstack-kubernetes/k3s-cluster/main.tf
+++ deployment/terraform/examples/openstack-kubernetes/k3s-cluster/main.tf
@@ -13,8 +13,8 @@ module "openstack_cogstack_infra" {
       # }
     },
   ]
-  allowed_ingress_ips_cidr    = var.allowed_ingress_ips_cidr
-  ubuntu_immage_name          = var.ubuntu_immage_name
+  allowed_ingress_ips_cidr = var.allowed_ingress_ips_cidr
+  ubuntu_immage_name       = var.ubuntu_immage_name
   # generate_random_name_prefix = false
   # prefix                      = "dev"
   # network = {
--- deployment/terraform/modules/openstack-kubernetes-infra/shared-locals.tf
+++ deployment/terraform/modules/openstack-kubernetes-infra/shared-locals.tf
@@ -11,23 +11,23 @@ locals {
   controller_host_has_floating_ip = local.controller_host.floating_ip != null && local.controller_host.floating_ip.use_floating_ip
   created_controller_host         = openstack_compute_instance_v2.kubernetes_server
   controller_host_instance = {
-    name        = local.controller_host.name
-    ip_address  = local.controller_host_has_floating_ip ? local.controller_host.floating_ip.address : openstack_compute_instance_v2.kubernetes_server.access_ip_v4
-    unique_name = local.created_controller_host.name
-    use_floating_ip = local.controller_host_has_floating_ip
+    name                = local.controller_host.name
+    ip_address          = local.controller_host_has_floating_ip ? local.controller_host.floating_ip.address : openstack_compute_instance_v2.kubernetes_server.access_ip_v4
+    unique_name         = local.created_controller_host.name
+    use_floating_ip     = local.controller_host_has_floating_ip
     internal_ip_address = openstack_compute_instance_v2.kubernetes_server.access_ip_v4
   }
 
-  created_nodes = { 
-    for node in var.host_instances : 
+  created_nodes = {
+    for node in var.host_instances :
     node.name => {
-      ip_address  = node.floating_ip != null && node.floating_ip.use_floating_ip ? node.floating_ip.address : openstack_compute_instance_v2.kubernetes_nodes[node.name].access_ip_v4
-      unique_name = openstack_compute_instance_v2.kubernetes_nodes[node.name].name
-      name        = node.name
-      use_floating_ip = node.floating_ip != null && node.floating_ip.use_floating_ip
+      ip_address          = node.floating_ip != null && node.floating_ip.use_floating_ip ? node.floating_ip.address : openstack_compute_instance_v2.kubernetes_nodes[node.name].access_ip_v4
+      unique_name         = openstack_compute_instance_v2.kubernetes_nodes[node.name].name
+      name                = node.name
+      use_floating_ip     = node.floating_ip != null && node.floating_ip.use_floating_ip
       internal_ip_address = openstack_compute_instance_v2.kubernetes_nodes[node.name].access_ip_v4
-    } 
-    if !node.is_controller 
+    }
+    if !node.is_controller
   }
 
 }
